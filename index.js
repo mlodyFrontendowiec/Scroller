b8f049878220187106888a651a6c66a36b4570a8
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2020-12-16 21:56:19 +0100

    Add scroll functionality

--- index.js
+++ index.js
@@ -2,9 +2,31 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log("załadowano");
   const rootElement = document.querySelector("#root");
   const sections = document.querySelectorAll("section");
+  let currentSectionIndex = 0;
+  let isThrottled = false;
 
   document.addEventListener("mousewheel", function (e) {
-    console.log(e.wheelDelta);
+    if (isThrottled) return;
+    isThrottled = true;
+
+    setTimeout(function () {
+      isThrottled = false;
+    }, 1000);
+
+    const direction = e.wheelDelta < 0 ? 1 : -1;
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) return;
+    } else if (direction === -1) {
+      const firstSection = currentSectionIndex === 0;
+      if (firstSection) return;
+    }
+    currentSectionIndex += direction;
+    console.log(currentSectionIndex);
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
   });
 
   console.log(sections);
