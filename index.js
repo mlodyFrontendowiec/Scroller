9a03ff1890fe282c7cf265ce67b40af4a48703a6
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2020-12-17 16:30:42 +0100

    Add support for keyboard

--- index.js
+++ index.js
@@ -3,4 +3,16 @@ document.addEventListener("DOMContentLoaded", function () {
   document.addEventListener("wheel", (event) => scroller.listenScroll(event));
   document.addEventListener("swipeUp", () => scroller.scroll(1));
   document.addEventListener("swipeDown", () => scroller.scroll(-1));
+  document.addEventListener("keydown", (e) => {
+    switch (e.keyCode) {
+      case 40:
+        return scroller.scroll(1);
+        break;
+      case 38:
+        return scroller.scroll(-1);
+        break;
+      default:
+        return;
+    }
+  });
 });
